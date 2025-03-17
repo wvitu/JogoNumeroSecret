bc3210c90bb73ad20087d5acbd41ba99f9875ceb
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-03-17 19:28:36 -0300

    Correção de tentativas para aprecer no alert

--- app.js
+++ app.js
@@ -9,17 +9,14 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10')
     // Se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}`)
+        alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`)
     } else {
         if(chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`)
         } else {
             alert(`O número secreto é maior que ${chute}`)
         }
-        // Para atribuir mais 1 é só adicionar ++
         // ex tentativas = tentativas + 1
         tentativas++ 
-
     }
 }
-console.log 
